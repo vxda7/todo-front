cf089534da60511dab5a5d5ca60c6ea6ceb70836
vxda77 <vxda77@gmail.com>
2019-11-19 10:19:29 +0900

    jwt session에 저장

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,20 +1,52 @@
 <template>
-  <div class="login-div col-6 offset-3">
-    <div class="form-group">
-      <label for="id">ID</label>
-      <input id="id" class="form-control" type="text">
+  <div>
+    <div v-if="loading" class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
     </div>
-    <div class="form-group">
-      <label for="password">PASSWORD</label>
-      <input id="password" class="form-control" type="password">
+    <div v-else class="login-div col-6 offset-3">
+      <div class="form-group">
+        <label for="id">ID</label>
+        <input id="id" class="form-control" type="text" v-model="credential.username">
+      </div>
+      <div class="form-group">
+        <label for="password">PASSWORD</label>
+        <input id="password" class="form-control" type="password" v-model="credential.password">
+      </div>
+      <button class="btn btn-primary" @click="login">로그인</button>
     </div>
-    <button class="btn btn-primary">로그인</button>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import router from '../router'
 
+export default {
+  data(){
+    return {
+      credential: {
+        username: '',
+        password: ''
+      },
+      loading: false
+    }
+  },
+  methods: {
+    login(){
+      console.log('로그인 시도!!!')
+      axios.post('http://localhost:8000/api-token-auth/', this.credential)
+      .then((res)=>{
+        this.loading = true
+        this.$session.start()
+        this.$session.set('jwt', res.data.token)
+        router.push('/')
+      })
+      .catch((err)=>{
+        this.loading = true
+        console.log(err)
+      })
+    },
+  }
 }
 </script>
 
