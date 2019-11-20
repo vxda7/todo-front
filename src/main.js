68fd5c480202f866249b66999dec9b7d7e1de9ce
vxda77 <vxda77@gmail.com>
2019-11-20 09:32:10 +0900

    vuex 설치

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueSession from 'vue-session'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -9,5 +10,6 @@ Vue.use(VueSession)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
